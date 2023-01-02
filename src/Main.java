a22a8b7b93e2d82e6031b7b6952d04e3ae4b7b2a
Noah Roberts <noahkrob97@gmail.com>
2023-01-01 18:38:01 -0600

    feat: Can now evaluate complex expressions with functions and parentehsis

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import tree.ExpressionTree;
 
 public class Main {
     public static void main(String[] args) {
-
-        String expression = "(sin(x))";
+        String expression = "(1 - sin(cos(pi + 2) * (1/212.5)) + 7)^4";
         ExpressionTree t = new ExpressionTree(expression);
         System.out.println(t.toString());
         System.out.println(t.printTokens());
-        System.out.println("ANSWER: " + t.solve(1));
+        System.out.println(t.solve());
     }
     
 } 
