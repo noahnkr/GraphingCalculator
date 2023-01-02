fafd62dde7ef0d0bd3463e8b7866579d916b54b7
Noah Roberts <noahkrob97@gmail.com>
2023-01-01 22:56:24 -0600

    WIP: simplified subtokens, build tree does not work

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ import tree.ExpressionTree;
 
 public class Main {
     public static void main(String[] args) {
-        String expression = "(1 - sin(cos(pi + 2) * (1/212.5)) + 7)^4";
+  
+        String expression = "sin(cos(x) + 4)";
         ExpressionTree t = new ExpressionTree(expression);
         System.out.println(t.toString());
         System.out.println(t.printTokens());
-        System.out.println(t.solve());
+        System.out.println(t.solve(Math.PI));
     }
     
 } 
